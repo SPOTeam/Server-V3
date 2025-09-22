4fc361ab91723911e194b34fef9633f5faf09441
msk226 <kminseok5167@gmail.com>
2025-09-22 16:25:32 +0900

    [Feature] 토큰 재발급 관련 Swagger 명세 추가

--- modules/auth/src/main/java/kr/spot/presentation/AuthController.java
+++ modules/auth/src/main/java/kr/spot/presentation/AuthController.java
@@ -1,5 +1,7 @@
 package kr.spot.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.spot.ApiResponse;
 import kr.spot.application.token.TokenReissueService;
 import kr.spot.code.status.SuccessStatus;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -18,6 +21,10 @@ public class AuthController {
 
     private final TokenReissueService tokenReissueService;
 
+    @Operation(summary = "토큰 재발급", description = """
+            ## 리프레시 토큰을 통해 새로운 액세스 토큰과 리프레시 토큰을 발급받습니다.
+            - **refreshToken**: 헤더에 담아 보내주세요. \n
+            """)
     @PostMapping("/reissue")
     public ResponseEntity<ApiResponse<TokenDTO>> reissueToken(
             @RequestHeader String refreshToken
